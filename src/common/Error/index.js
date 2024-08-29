29f78d36ac77eb82d4ca16eef76f4d6ce9ff675f
Adam Musial <adammusial@wp.pl>
2024-08-29 18:48:34 +0200

    placing a component in a container

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -1,3 +1,4 @@
+import { Container } from "../Container/styled";
 import {
   StyledButtonLink,
   ErrorMessage,
@@ -11,21 +12,24 @@ import {
 } from "./styled";
 
 export const Error = () => (
-  <Wrapper>
-    <IconWrapper>
-      <StyledWarningIcon />
-      <StyledExclamationIcon />
-    </IconWrapper>
-    <ErrorMessage>
-      <ErrorHeader>Ooops! Something went wrong...</ErrorHeader>
-      <ErrorInfo>
-        Please check your network connection <br /> and try again
-      </ErrorInfo>
-      <StyledButtonLink to="/movies">
-        <ButtonText>
-          Back to home page
-        </ButtonText>
-      </StyledButtonLink>
-    </ErrorMessage>
-  </Wrapper>
+  <Container>
+    <Wrapper>
+      <IconWrapper>
+        <StyledWarningIcon />
+        <StyledExclamationIcon />
+      </IconWrapper>
+      <ErrorMessage>
+        <ErrorHeader>Ooops! Something went wrong...</ErrorHeader>
+        <ErrorInfo>
+          Please check your network connection <br /> and try again
+        </ErrorInfo>
+        <StyledButtonLink to="/movies">
+          <ButtonText>
+            Back to home page
+          </ButtonText>
+        </StyledButtonLink>
+      </ErrorMessage>
+    </Wrapper>
+  </Container>
+
 );
