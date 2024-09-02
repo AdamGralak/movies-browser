5308c0f3c6dbec440b7ae912df9340f95d0be298
Adam Musial <adammusial@wp.pl>
2024-09-02 21:55:06 +0200

    creation of a page navigation function

--- src/common/Paginator/index.js
+++ src/common/Paginator/index.js
@@ -1,18 +1,37 @@
-import React from "react";
-import { StyledPaginator } from "./styled";
+import { StyledPaginator, Button, PageInfo } from "./styled";
 
-const Paginator = () => {
+const Paginator = ({ totalPages, currentPage, onPageChange }) => {
 
-    return(
-<>
-<StyledPaginator>
-<button>Przejdź do pierwszej strony</button>
-<button>Poprzednia strona</button>
-<span> aktualna strona: </span>
-<button>Następna strona</button>
-<button>Przejdź do ostatniej strony</button>
-</StyledPaginator>
-</>
+    const handleFirstPage = () => {
+        onPageChange(1);
+    };
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            onPageChange(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            onPageChange(currentPage + 1);
+        }
+    };
+
+    const handleLastPage = () => {
+        onPageChange(totalPages);
+    };
+
+    return (
+        <StyledPaginator>
+            <Button onClick={handleFirstPage} disabled={currentPage === 1}>Przejdź do pierwszej strony</Button>
+            <Button onClick={handlePreviousPage} disabled={currentPage === 1}>Poprzednia strona</Button>
+            <PageInfo>
+                Page {currentPage} of {totalPages}
+            </PageInfo>
+            <Button onClick={handleNextPage} disabled={currentPage === totalPages}>Następna strona</Button>
+            <Button onClick={handleLastPage} disabled={currentPage === totalPages}>Przejdź do ostatniej strony</Button>
+        </StyledPaginator>
     );
 };
 
