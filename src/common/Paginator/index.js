8c598053b08abcdb5fc63064d33de4a7eae4bffa
Adam Musial <adammusial@wp.pl>
2024-09-08 21:04:24 +0200

    adding paginator

--- src/common/Paginator/index.js
+++ src/common/Paginator/index.js
@@ -1,18 +1,81 @@
-import React from "react";
-import { StyledPaginator } from "./styled";
+import { useState, useEffect } from 'react';
+import { useTheme } from 'styled-components';
+import {
+    StyledPaginator,
+    PaginatorButton,
+    ButtonImage,
+    PageInfo,
+    PageText,
+    PageNumber,
+    BackwardForward
+} from "./styled";
+import FirstPageIcon from './First.svg';
+import PreviousPageIcon from './Previous.svg';
+import NextPageIcon from './Next.svg';
+import LastPageIcon from './Last.svg';
+import MobileFirstPageIcon from './MobileFirst.svg';
+import MobilePreviousPageIcon from './MobilePrevious.svg';
+import MobileNextPageIcon from './MobileNext.svg';
+import MobileLastPageIcon from './MobileLast.svg';
 
 const Paginator = () => {
+    const theme = useTheme();
+    const mobileMax2 = theme.breakpoint.mobileMax2;
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= mobileMax2);
 
-    return(
-<>
-<StyledPaginator>
-<button>Przejdź do pierwszej strony</button>
-<button>Poprzednia strona</button>
-<span> aktualna strona: </span>
-<button>Następna strona</button>
-<button>Przejdź do ostatniej strony</button>
-</StyledPaginator>
-</>
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= mobileMax2);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [mobileMax2]);
+
+    {/*const handleFirstPage = () => {
+        onPageChange(1);
+    };
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            onPageChange(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            onPageChange(currentPage + 1);
+        }
+    };
+
+    const handleLastPage = () => {
+        onPageChange(totalPages);
+    };*/}
+
+    return (
+        <StyledPaginator>
+            <BackwardForward>
+                <PaginatorButton> {/*onClick={handleFirstPage} disabled={currentPage === 1*/} 
+                    <ButtonImage src={isMobile ? MobileFirstPageIcon : FirstPageIcon} alt="First page" />
+                </PaginatorButton>
+                <PaginatorButton> {/*onClick={handlePreviousPage} disabled={currentPage === 1}*/}
+                    <ButtonImage src={isMobile ? MobilePreviousPageIcon : PreviousPageIcon} alt="Previous page" />
+                </PaginatorButton>
+            </BackwardForward>
+            <PageInfo>
+                <PageText>Page</PageText>
+                <PageNumber>1</PageNumber>
+                <PageText>of</PageText>
+                <PageNumber>500</PageNumber>
+            </PageInfo>
+            <BackwardForward>
+                <PaginatorButton> {/*onClick={handleNextPage} disabled={currentPage === totalPages}*/}
+                    <ButtonImage src={isMobile ? MobileNextPageIcon : NextPageIcon} alt="Next page" />
+                </PaginatorButton>
+                <PaginatorButton> {/*onClick={handleLastPage} disabled={currentPage === totalPages}*/}
+                    <ButtonImage src={isMobile ? MobileLastPageIcon : LastPageIcon} alt="Last page" />
+                </PaginatorButton>
+            </BackwardForward>
+        </StyledPaginator>
     );
 };
 
