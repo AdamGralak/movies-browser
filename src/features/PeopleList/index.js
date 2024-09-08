f9b6e3a04fca4554fe3c6f69afc565e91241e262
Marcin Jankowski <marcin5771@gmail.com>
2024-09-08 17:54:53 +0200

    Handled case when role is not available

--- src/features/PeopleList/index.js
+++ src/features/PeopleList/index.js
@@ -18,7 +18,7 @@ export const PeopleList = ({ people = [], baseurl, renderinmoviedetails }) => {
                         <Photo photo={url} alt={person.name} />
                         <Wrapper>
                             <Name>{person.name}</Name>
-                            {renderinmoviedetails && <Role>{role}</Role>}
+                            {renderinmoviedetails && <Role>{role !== "undefined" ? role : ""}</Role>}
                         </Wrapper>
                     </StyledLink>
                 );
